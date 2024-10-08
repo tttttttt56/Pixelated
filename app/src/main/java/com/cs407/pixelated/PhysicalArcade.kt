570df8e6b1840a519b91ad1518825646acb60b2b
Tygro <tbgrogg@wisc.edu>
2024-10-07 21:37:56 -0500

    Start of wireframe

---
+++ app/src/main/java/com/cs407/pixelated/PhysicalArcade.kt
@@ -0,0 +1,20 @@
+package com.cs407.pixelated
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class PhysicalArcade : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_physical_arcade)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
