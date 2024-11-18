f8ca6c5f25db427969f9b0ebf231b5511d71344b
Sumit-1107 <singha2@wisc.edu>
2024-11-18 03:08:58 -0600

    updated the scoreboard ui, to include high score and most recent score (hard coded scores)

--- app/src/main/java/com/cs407/pixelated/MainActivity.kt
+++ app/src/main/java/com/cs407/pixelated/MainActivity.kt
@@ -9,13 +9,11 @@ import android.view.View
 import android.widget.Button
 import android.widget.ImageButton
 import android.widget.PopupMenu
-import androidx.appcompat.widget.Toolbar
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import androidx.navigation.NavController
-import androidx.navigation.findNavController
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
